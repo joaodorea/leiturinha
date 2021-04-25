b48a774f0606762f856ec225cde3b3ce2adeedf3
Joao Dorea <joaopdorea@gmail.com>
2021-04-25 16:00:42 -0300

    feat: showing total in order summary

--- src/modules/orders/views/index.tsx
+++ src/modules/orders/views/index.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '@/models/Product'
 import ProductsOrder from '@/models/ProductsOrder'
 
 import ItemsMenu from '../components/itemsMenu'
 import productsList from '@/assets/mocks/products.json'
+import utils from '@/utils/index'
 
 const Orders: React.FC = () => {
   const [ order, setOrder ] = useState<ProductsOrder[]>([])
+  const [ total, setTotal ] = useState(0)
   const products: Product[] = productsList as Product[]
+
+  useEffect(() => {
+    setTotal(() => order.reduce((init: number, actual: ProductsOrder): number => {
+      const product = products.find(p => p.id === actual.product_id)
+      init += product.price * actual.quantity
+
+      return init
+    }, 0))
+  }, [ order ])
   
   return (
     <>
@@ -20,6 +31,8 @@ const Orders: React.FC = () => {
           return <li key={product.id}>{item.quantity}x {product.name}</li>
         })}
       </ul>
+
+      <h2>Total {utils.formatMoney(total)}</h2>
     </>
   )
 }
